74cc335208f90d2db520baeb9d0e36c55d5d7eec
Le Dam Duy Phuc <permees@nexondv.com>
2024-05-23 09:10:07 +0700

    feat: 26

---
+++ 26.remove-duplicates-from-sorted-array/index.ts
@@ -0,0 +1,9 @@
+function removeDuplicates(nums: number[]): number {
+  let i = 0;
+  while (i < nums.length - 1) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+    } else i++;
+  }
+  return nums.length;
+}
