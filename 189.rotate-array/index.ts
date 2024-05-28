97c6cd602c61ba834839972490e9adced825497f
Le Dam Duy Phuc <permees@nexondv.com>
2024-05-28 09:03:33 +0700

    feat: 189

--- 189.rotate-array/index.ts
+++ 189.rotate-array/index.ts
@@ -1,12 +1,11 @@
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {void} Do not return anything, modify nums in-place instead.
+ Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
+function rotate(nums: number[], k: number): void {
   while (k > nums.length) k -= nums.length;
+
   const res = [...nums.slice(-k), ...nums.slice(0, -k)];
   for (let i = 0; i < nums.length; i++) {
     nums[i] = res[i];
   }
-};
+}
