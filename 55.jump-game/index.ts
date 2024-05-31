ca1c3ca332fb45a979eecf44d967108c81c6596c
Le Dam Duy Phuc <permees@nexondv.com>
2024-05-31 14:13:40 +0700

    feat: 55

---
+++ 55.jump-game/index.ts
@@ -0,0 +1,9 @@
+function canJump(nums: number[]): boolean {
+  let max = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > max) return false;
+    max = Math.max(max, i + nums[i]);
+  }
+  return true;
+}
+canJump([2, 3, 1, 1, 4]);
---
+++ math.max.js
@@ -0,0 +1,36 @@
+const perfTestAmount = 1000000000;
+function perfTestMax() {
+  const n = "math";
+  console.time(n);
+  let m = 0;
+  let l = perfTestAmount;
+  while (l--) {
+    m = Math.max(m, Math.random());
+  }
+  console.timeEnd(n);
+}
+function perfTestMaxIf() {
+  const n = "if";
+  console.time(n);
+  let m = 0;
+  let l = perfTestAmount;
+  while (l--) {
+    const r = Math.random();
+    if (m < r) m = r;
+  }
+  console.timeEnd(n);
+}
+function perfTestMaxQ() {
+  const n = "qst";
+  console.time(n);
+  let m = 0;
+  let l = perfTestAmount;
+  while (l--) {
+    const r = Math.random();
+    m = m < r ? r : m;
+  }
+  console.timeEnd(n);
+}
+perfTestMaxIf();
+perfTestMaxQ();
+perfTestMax();
